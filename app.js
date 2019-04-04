90e9f103db303203e0a104834b2fc1a10c313666
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-04-04 18:33:01 +0530

    Added inserting records

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const db = require('./database');
 
 const select_all = async () => await db.execute('SELECT * FROM products');
+const insert_into = async (val) => await db.execute(`INSERT INTO products (title, price, emoji) VALUES(${val})`);
 
-(async () => {
-    try {
-        let [[result]] = await select_all();
-        console.log(result);
-    } catch (e) {
+// (async () => {
+//     try {
+//         let [results, metadata] = await select_all();
+//         Object.assign(results[0], {viewed:true}); // or simply results[0].viewed=true
+//         console.log(results[0]);
+//     } catch (e) {
+//         console.log(e);
+//     }
+// })();
+
+(async()=> {
+    try{
+        let results = await insert_into("'Lollipop', '2.50', '🍭'");
+        console.log("Affected rows: ", results[0].affectedRows);
+    }catch(e){
         console.log(e);
     }
 })();
